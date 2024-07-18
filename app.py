78742c35efc3a724d04f542d7b1e26fbcae14ffa
Aurelien Blond <aurelien.blond@gmail.com>
2024-07-18 16:07:19 +0200

    Test update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 
 '''
 # TaxiFareModel front
-
+ADD SOMTEHING STUPID
 This front queries the Le Wagon [taxi fare model API](https://taxifare.lewagon.ai/predict?pickup_datetime=2012-10-06%2012:10:20&pickup_longitude=40.7614327&pickup_latitude=-73.9798156&dropoff_longitude=40.6513111&dropoff_latitude=-73.8803331&passenger_count=2)
 '''
 
